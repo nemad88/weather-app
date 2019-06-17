2f1d8b71fb07035c1903f243d09d37c4ffaa606e
Adam Nemeth <nemethadam88@gmail.com>
2019-06-17 20:42:34 +0200

    added routes

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const app = express()
+
+app.get('', (req, res) => {
+    res.send('Hello express')
+})
+
+app.get('/help', (req, res) => {
+    res.send('Help page')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About page')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('Weather page')
+})
+
+
+app.listen(3333, () => {
+    console.log('Server is up port 3333')
+})
